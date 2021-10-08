5fc5d90d55a332d9eb92e302b5980b714c1ce87c
ashwat <ashwat.acharya09@gmail.com>
2021-10-08 20:55:28 +0530

    tsconfig compilerOptions changes for @emotion/react

--- components/header/style.ts
+++ components/header/style.ts
@@ -1,21 +1,20 @@
 import { css } from '@emotion/react'
 
-const SR_ONLY = `
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-`
+// const SR_ONLY = `
+//   clip: rect(0 0 0 0);
+//   clip-path: inset(50%);
+//   height: 1px;
+//   overflow: hidden;
+//   position: absolute;
+//   white-space: nowrap;
+//   width: 1px;
+// `
 
 const container = css`
   min-height: 100vh;
   display: grid;
   place-items: center;
   padding: 0 0.5rem;
-  /* background-color: var(--background-accent); */
 `
 // const title = css`
 //   ${SR_ONLY}
@@ -24,8 +23,6 @@ const container = css`
 const title = css`
   font-size: 4.5rem;
   text-align: center;
-  /* padding: 0 1ch;
-  color: var(--white); */
 `
 
 export { container, title }
